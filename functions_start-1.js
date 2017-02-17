5cbd01a44e488e7560b1a396a60a47f6be11713e
Jorge Romero <jorgeromero@Jorges-MacBook-Air.local>
2017-02-17 13:26:33 -0800

    completed the code

--- functions_start-1.js
+++ functions_start-1.js
@@ -25,7 +25,7 @@ function listTriangularNumbers(n) {
     var array = [];
     for (i = 1; i <= n; ++i) {
         num = i;
-        for (j = i; j >= 1; --j) {
+        for (j = i - 1; j >= 1; --j) {
             num = num + j;
         }
         array.push(num);
@@ -42,9 +42,9 @@ function listTriangularNumbers(n) {
 function multTable(row, col) {
     var table = "<table>";
     var x;
-    for (var i = 1; i < col; ++i) {
+    for (var i = 1; i <= row; ++i) {
         table = table + "<tr>";
-        for (var j = 1; j < row; ++j) {
+        for (var j = 1; j <= col; ++j) {
             x = i * j;
             table = table + "<td>" + x + "</td>";
         }
@@ -60,7 +60,7 @@ function multTable(row, col) {
  * @returns {number}
  */
 function factorial(n) {
-    var result = 0;
+    var result = 1;
     for (var i = n; i > 1; i--) {
         result = result * i;
     }
@@ -75,7 +75,7 @@ function factorial(n) {
  */
 function combinations(n, k) {
     var c;
-    c = factorial(n) / factorial(k) * (factorial(n - k));
+    c = factorial(n) / (factorial(k) * (factorial(n - k)));
     return c;
 }
 
@@ -91,9 +91,9 @@ function sort(a) {
         var k;
         for (k = i - 1;
             (k >= 0) && (j < result[k]); k--) {
-            result[k - 1] = result[k];
+            result[k + 1] = result[k];
         }
-        result[k - 1] = j;
+        result[k + 1] = j;
     }
     return result;
 }
